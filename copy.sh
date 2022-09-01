afcecbd8e97fe831143e191f94df6dfe5ea95b00
Christopher Aubuchon <caubuchon@petrichor.io>
2022-09-01 15:40:52 -0700

    internal api added

--- copy.sh
+++ copy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash 
 
-cp ../cycle-openapi/dist/out.yml ./api/out.yml
+cp ../cycle-openapi/dist/public-api.yml ./api/public-api.yml
+cp ../cycle-openapi/dist/internal-api.yml ./api/internal-api.yml
